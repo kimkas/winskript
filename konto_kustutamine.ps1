8a26d72bd52a8b97a7d121753869a5dba673f9ad
kimkas <115539749+kimkas@users.noreply.github.com>
2023-01-11 09:44:41 +0200

    konto kustutamine (ees, perenimi)

---
+++ konto_kustutamine.ps1
@@ -0,0 +1,17 @@
+# Skript kustutab kasutaja tema ees ja perenime põhjal
+Echo "Sisesta eesnimi"
+$eesnimi=Read-Host
+Echo "Sisesta oma perekonnanimi"
+$perenimi=Read-Host
+$kasutajanimi=$eesnimi.ToLower() + "." + $perenimi.ToLower()
+$ErrorActionPreference = "SilentlyContinue"
+Remove-LocalUser "$kasutajanimi"
+if(!$?)
+{
+Echo "Viga! kontot ei eksisteeri."
+}
+else
+{
+Echo "Konto $kasutajanimi on kustutatud"
+}
+$ErrorActionPreference = "Stop!"
